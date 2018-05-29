99067f029ae57e2829f8edd06d6f1c7eddf7d2bb
Mrinali Rao <mrinali.rao@safetyculture.io>
2018-05-29 10:09:09 +1000

    created a new button

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import styled from 'styled-components';
+import Button from './components/Button';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <Button> Save </Button>
+      </div>
+    );
+  }
+}
+
+export default App;
